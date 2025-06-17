cb121e727c690b8b28fb962cb041f958011b3fdf
Kiet <githu-dev@kt.mozmail.com>
2025-06-17 20:04:04 +0200

    improvements to autocompletion

--- .config/nvim/lua/plugins/autocompletion.lua
+++ .config/nvim/lua/plugins/autocompletion.lua
@@ -26,28 +26,77 @@ return {
 					end
 					return "make install_jsregexp"
 				end)(),
-				dependencies = {},
+				dependencies = {
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
 				opts = {},
 			},
-			"folke/lazydev.nvim",
 		},
 		opts = {
 			keymap = {
-				preset = "default",
+				-- 'default' | 'super-tab' | 'enter' | 'none'
+				preset = "enter",
 				["<C-j>"] = { "show_and_insert", "select_next" },
 				["<C-k>"] = { "show_and_insert", "select_prev" },
-				["<C-CR>"] = { "select_and_accept" },
+			},
+
+			appearance = {
+				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
+
+			completion = {
+				menu = {
+					draw = {
+						columns = {
+							{
+								"kind_icon",
+								"label",
+								"label_description",
+								"source_name",
+								gap = 1,
+							},
+							components = {
+								label_description = {
+									width = { max = 50 },
+								},
+								source_name = {
+									text = function(ctx)
+										return "[" .. ctx.source_name .. "]"
+									end,
+								},
+							},
+						},
+					},
+				},
+				-- By default, you may press `<c-space>` to show the documentation.
+				-- Optionally, set `auto_show = true` to show the documentation after a delay.
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 500,
+				},
 			},
 
 			sources = {
-				default = { "lsp", "path", "snippets", "lazydev" },
+				default = { "lsp", "lazydev", "path", "snippets", "buffer" },
 				providers = {
+					lsp = { score_offset = 1000 },
+					buffer = { score_offset = 800 },
+					path = { score_offset = 800 },
+					snippets = { score_offset = 900 },
 					lazydev = {
+						name = "LazyDev",
 						module = "lazydev.integrations.blink",
 						score_offset = 100,
 					},
 				},
 			},
+
+			snippets = { preset = "luasnip" },
 
 			fuzzy = { implementation = "lua" },
 
