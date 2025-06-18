713204e081a23c9e6bfeb31fafe58affa5245c91
Kiet van Wijk <kiet@opzet.nl>
2025-06-18 11:50:50 +0200

    improve completions

--- .config/nvim/lua/plugins/autocompletion.lua
+++ .config/nvim/lua/plugins/autocompletion.lua
@@ -1,106 +1,129 @@
 return {
-	"folke/lazydev.nvim",
-	ft = "lua",
-	opts = {
-		library = {
-			{
-				path = "${3rd}/luv/library",
-				words = { "vim%.uv" },
-			},
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"saadparwaiz1/cmp_luasnip",
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = "rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
 		},
 	},
-	{
-		"saghen/blink.cmp",
-		event = "VimEnter",
-		dependencies = {
-			-- Snippet Engine
-			{
-				"L3MON4D3/LuaSnip",
-				version = "2.*",
-				build = (function()
-					-- Build Step is needed for regex support in snippets.
-					-- This step is not supported in many windows environments.
-					-- Remove the below condition to re-enable on windows.
-					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-						return
-					end
-					return "make install_jsregexp"
-				end)(),
-				dependencies = {
-					"rafamadriz/friendly-snippets",
-					config = function()
-						require("luasnip.loaders.from_vscode").lazy_load()
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+		print(vim.inspect(cmp.get_config().sources))
+
+		-- Automatically add parentheses on completion
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+
+			mapping = cmp.mapping.preset.insert({
+				["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+				["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				-- Confirm if there's a selection, else insert new line.
+				["<CR>"] = cmp.mapping({
+					i = function(fallback)
+						if cmp.visible() and cmp.get_active_entry() then
+							cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+						else
+							fallback()
+						end
 					end,
+					s = cmp.mapping.confirm({ select = true }),
+					c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+				}),
+				-- Confirm selected entry, else confirm first available entry.
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						local entry = cmp.get_selected_entry()
+						if not entry then
+							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+						end
+						cmp.confirm()
+					else
+						fallback()
+					end
+				end, { "i", "s", "c" }),
+			}),
+
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp", priority = 1000 },
+				{ name = "luasnip", priority = 750 },
+				{ name = "buffer", priority = 500 },
+				{ name = "path", priority = 250 },
+			}),
+
+			sorting = {
+				priority_weight = 2,
+				comparators = {
+					cmp.config.compare.offset,
+					cmp.config.compare.exact,
+					cmp.config.compare.score,
+					cmp.config.compare.kind,
+					cmp.config.compare.sort_text,
+					cmp.config.compare.length,
+					cmp.config.compare.order,
 				},
-				opts = {},
-			},
-		},
-		opts = {
-			keymap = {
-				-- 'default' | 'super-tab' | 'enter' | 'none'
-				preset = "enter",
-				["<C-j>"] = { "show_and_insert", "select_next" },
-				["<C-k>"] = { "show_and_insert", "select_prev" },
 			},
 
-			appearance = {
-				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				-- Adjusts spacing to ensure icons are aligned
-				nerd_font_variant = "mono",
+			formatting = {
+				format = function(entry, vim_item)
+					vim_item.menu = ({
+						nvim_lsp = "[LSP]",
+						luasnip = "[Snippets]",
+						buffer = "[Buffer]",
+						path = "[Path]",
+						cmdline = "[Cmd]",
+					})[entry.source.name] or ("[" .. entry.source.name .. "]")
+					return vim_item
+				end,
 			},
 
+			-- TODO find a away to add a delay.
 			completion = {
-				menu = {
-					draw = {
-						columns = {
-							{
-								"kind_icon",
-								"label",
-								"label_description",
-								"source_name",
-								gap = 1,
-							},
-							components = {
-								label_description = {
-									width = { max = 50 },
-								},
-								source_name = {
-									text = function(ctx)
-										return "[" .. ctx.source_name .. "]"
-									end,
-								},
-							},
-						},
-					},
-				},
-				-- By default, you may press `<c-space>` to show the documentation.
-				-- Optionally, set `auto_show = true` to show the documentation after a delay.
-				documentation = {
-					auto_show = true,
-					auto_show_delay_ms = 500,
-				},
+				popup_delay = 200,
 			},
 
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+		})
+
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
 			sources = {
-				default = { "lsp", "lazydev", "path", "snippets", "buffer" },
-				providers = {
-					lsp = { score_offset = 1000 },
-					buffer = { score_offset = 800 },
-					path = { score_offset = 800 },
-					snippets = { score_offset = 900 },
-					lazydev = {
-						name = "LazyDev",
-						module = "lazydev.integrations.blink",
-						score_offset = 100,
-					},
-				},
+				{ name = "buffer" },
 			},
+		})
 
-			snippets = { preset = "luasnip" },
-
-			fuzzy = { implementation = "lua" },
-
-			signature = { enabled = true },
-		},
-	},
+		-- TDOO enable cml completions but disabled auto complete
+		-- cmp.setup.cmdline(":", {
+		-- 	mapping = cmp.mapping.preset.cmdline(),
+		-- 	sources = cmp.config.sources({
+		-- 		{ name = "path" },
+		-- 	}, {
+		-- 		{ name = "cmdline" },
+		-- 	}),
+		-- })
+	end,
 }
