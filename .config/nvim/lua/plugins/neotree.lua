5b5f705c9f5a62f18ae399f370c0f20658f1fe43
Kiet van Wijk <kiet@opzet.nl>
2025-07-15 09:03:05 +0200

    auto close neotree on file open

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -290,6 +290,14 @@ return {
 					},
 				},
 			},
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						vim.cmd([[Neotree close]])
+					end,
+				},
+			},
 		})
 
 		vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
