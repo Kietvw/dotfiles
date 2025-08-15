447bb5ba932fbf7b64b14dd0f85133f2ae7c4f5f
Kiet van Wijk <kiet@opzet.nl>
2025-08-15 11:17:11 +0200

    format

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,12 +1,12 @@
 vim.pack.add({
-    "https://github.com/stevearc/oil.nvim"
+	"https://github.com/stevearc/oil.nvim",
 })
 
-require "oil".setup({
-    default_file_explorer = true,
-    view_options = {
-        show_hidden = true,
-    },
+require("oil").setup({
+	default_file_explorer = true,
+	view_options = {
+		show_hidden = true,
+	},
 })
 
-vim.keymap.set('n', '<leader>e', ":Oil<CR>", { silent = true })
+vim.keymap.set("n", "<leader>e", ":Oil<CR>", { silent = true })
