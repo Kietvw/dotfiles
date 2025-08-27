470949ad8557393292db9fc370d631b39b8be1f5
Kiet van Wijk <kiet@opzet.nl>
2025-08-27 13:52:18 +0200

    change oil keymaps

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,10 +2,20 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/oil.nvim", version = vim.version.range("2.15") },
 })
 
+local actions = require("oil.actions")
+
 require("oil").setup({
 	default_file_explorer = true,
 	view_options = {
 		show_hidden = true,
+	},
+	keymaps = {
+		["<C-h>"] = false,
+		["<C-j>"] = false,
+		["<C-k>"] = false,
+		["<C-l>"] = false,
+		["<leader>po"] = actions.open_terminal,
+		["<leader>e"] = actions.refresh,
 	},
 })
 
